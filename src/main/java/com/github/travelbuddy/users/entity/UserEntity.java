aa64dda9de5e60b50636d32b1c242fb64f748421
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-16 16:51:04 +0900

    Feat: UserEntity 생성

---
+++ src/main/java/com/github/travelbuddy/users/entity/UserEntity.java
@@ -0,0 +1,56 @@
+package com.github.travelbuddy.users.entity;
+
+import com.github.travelbuddy.users.enums.Gender;
+import com.github.travelbuddy.users.enums.Role;
+import com.github.travelbuddy.users.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "email", length = 40, nullable = false)
+    private String email;
+
+    @Column(name = "password", length = 100, nullable = false)
+    private String password;
+
+    @Column(name = "resident_num", nullable = false)
+    private Integer residentNum;
+
+    @Column(name = "phone_num", length = 20)
+    private String phoneNum;
+
+    @Column(name = "gender", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    //todo: 기본 사진
+    @Column(name = "profile_picture_url", length = 255, columnDefinition = "VARCHAR(255) DEFAULT ''")
+    private String profilePictureUrl;
+
+    @Column(name = "status", columnDefinition = "ENUM('ACTIVE', 'DELETED') DEFAULT 'ACTIVE'")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "role", columnDefinition = "ENUM('USER', 'ALL') DEFAULT 'USER'")
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    //todo 시간
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+}
