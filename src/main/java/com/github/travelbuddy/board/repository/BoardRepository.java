b8dba0436759342f1d2a3d7ebb030a2f0f7d1f16
usnimoj <jominsu0103@naver.com>
2024-07-17 16:16:05 +0900

    Feat : BoardRepository 전체조회 native query 작성

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -1,9 +1,15 @@
 package com.github.travelbuddy.board.repository;
 
 import com.github.travelbuddy.board.entity.BoardEntity;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity , Integer> {
+    @Query("SELECT b, pi.url AS representativeImage FROM BoardEntity b LEFT JOIN PostImageEntity pi ON b.id = pi.postId WHERE (:category IS NULL OR b.category = :category) GROUP BY b.id")
+    List<Object[]> findAllWithRepresentativeImage(String category, Sort sort);
 }
