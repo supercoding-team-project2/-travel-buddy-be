c47157d1451b367c02973e1e4929015265790da4
usnimoj <jominsu0103@naver.com>
2024-07-17 22:54:16 +0900

    Fix : 날짜데이터에서 time이 섞여들어와 Date타입으로 변환 수정

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.github.travelbuddy.board.repository;
 
 import com.github.travelbuddy.board.entity.BoardEntity;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,13 +11,13 @@ import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
-    @Query(value = "SELECT b.id ,b.category, b.title, b.summary, b.suggestion, u.name AS author, r.start_at, r.end_at, pi.url AS representative_image " +
+    @Query(value = "SELECT b.id, b.category, b.title, b.summary, b.suggestion, u.name AS author, r.start_at, r.end_at, pi.url AS representative_image " +
                    "FROM boards b " +
                    "LEFT JOIN users u ON b.user_id = u.id " +
                    "LEFT JOIN routes r ON b.route_id = r.id " +
                    "LEFT JOIN post_imgs pi ON b.id = pi.post_id " +
                    "WHERE (:category IS NULL OR b.category = :category) " +
-                   "AND (r.start_at BETWEEN :startDate AND :endDate OR r.end_at BETWEEN :startDate AND :endDate) " +
+                   "AND (:startDate IS NULL OR :endDate IS NULL OR (DATE(r.start_at) <= DATE(:startDate) AND DATE(r.end_at) >= DATE(:endDate))) " +
                    "AND pi.id = (SELECT pi2.id FROM post_imgs pi2 WHERE pi2.post_id = b.id ORDER BY pi2.id LIMIT 1) " +
                    "ORDER BY " +
                    "CASE WHEN :sortBy = 'createdAt' AND :order = 'desc' THEN b.created_at END DESC, " +
