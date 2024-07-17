b6a57f4be839cf8075cf612160e02a0588759309
usnimoj <jominsu0103@naver.com>
2024-07-17 16:00:37 +0900

    Fix : BoardRepository annotation 수정

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -2,6 +2,8 @@ package com.github.travelbuddy.board.repository;
 
 import com.github.travelbuddy.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BoardRepository extends JpaRepository<BoardEntity , Integer> {
 }
