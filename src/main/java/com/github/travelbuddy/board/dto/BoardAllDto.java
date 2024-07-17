9d994f755f64c122e40f907a5a339c1255db9e9b
usnimoj <jominsu0103@naver.com>
2024-07-17 20:13:58 +0900

    Fix : 전체조회 필요한 데이터 선별 후 dto 수정

--- src/main/java/com/github/travelbuddy/board/dto/BoardAllDto.java
+++ src/main/java/com/github/travelbuddy/board/dto/BoardAllDto.java
@@ -4,9 +4,18 @@ import com.github.travelbuddy.board.entity.BoardEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 @AllArgsConstructor
 public class BoardAllDto {
-    private BoardEntity board;
+    private Integer id;
+    private String categoryEnum;
+    private String title;
+    private String summary;
+    private Integer suggestion;
+    private String author;
+    private Date startAt;
+    private Date endAt;
     private String representativeImage;
 }
