b69e3f65202689c31aed635fba639952af627003
usnimoj <jominsu0103@naver.com>
2024-07-17 15:57:30 +0900

    Feat : BoardEntity 작성

---
+++ src/main/java/com/github/travelbuddy/board/entity/BoardEntity.java
@@ -0,0 +1,50 @@
+package com.github.travelbuddy.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "boards")
+@ToString
+public class BoardEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    // TODO : UserEntity 작성 후 수정
+    @Column(name = "user_id")
+    private Integer userId;
+
+    // TODO : RouteEntity 작성 후 수정
+    @Column(name = "route_id")
+    private Integer routeId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 255)
+    private String summary;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Category category;
+
+    @Column(columnDefinition = "int default 0")
+    private Integer like;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public enum Category {
+        REVIEW, COMPANION, GUIDE
+    }
+}
