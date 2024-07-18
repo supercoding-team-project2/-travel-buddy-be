94d5dc9b04e9d3237200b3571544951943f55b17
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-19 08:39:19 +0900

    Feat: JWT 생성,검증 기능 구현

---
+++ src/main/java/com/github/travelbuddy/users/jwt/JWTFilter.java
@@ -0,0 +1,64 @@
+package com.github.travelbuddy.users.jwt;
+
+import com.github.travelbuddy.users.dto.CustomUserDetails;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JWTFilter extends OncePerRequestFilter {
+
+    private final JWTUtill jwtUtill;
+
+@Override
+protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    System.out.println("JWTFilter");
+    String authorization = request.getHeader("Authorization");
+
+    //Authorization 헤더 검증
+    if(authorization == null || !authorization.startsWith("Bearer ")) {
+        System.out.println("token null");
+
+        //request,response를 다음 필터로 넘겨줌
+        filterChain.doFilter(request, response);
+
+        return;
+    }
+
+    //Bearer부분 제거
+    String token = authorization.split(" ")[1];
+    try {
+
+        if (jwtUtill.isExpired(token)) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        Integer userId = jwtUtill.getUserId(token);
+
+        System.out.println("userId: " + userId);
+        CustomUserDetails customUserDetails = CustomUserDetails.builder()
+                .userId(userId)
+                .build();
+
+        Authentication authToken = new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+    }catch (SignatureException e) {
+        System.err.println("Invalid token");
+    }
+        filterChain.doFilter(request, response);
+}
+}
---
+++ src/main/java/com/github/travelbuddy/users/jwt/JWTUtill.java
@@ -0,0 +1,45 @@
+package com.github.travelbuddy.users.jwt;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtill {
+
+    private final SecretKey secretKey;
+
+    private JWTUtill(@Value("${spring.jwt.secret}")String secret) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public Integer getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("userId", Integer.class);
+    }
+
+    public Boolean isExpired(String token) {
+        try {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+        } catch (ExpiredJwtException e) {
+            System.err.println("Token is expired");
+            return true;
+        }
+    }
+
+    public String createJwt(Integer userId, Long expiredMs){
+        return Jwts.builder()
+                .claim("userId",userId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
