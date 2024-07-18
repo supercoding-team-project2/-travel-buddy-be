efa388aa44dfb49158af22e1241966055801cfd2
usnimoj <jominsu0103@naver.com>
2024-07-18 19:20:10 +0900

    Feat : BoardController 게시글 상세조회 getPostDetails 작성

--- src/main/java/com/github/travelbuddy/common/controller/BoardController.java
+++ src/main/java/com/github/travelbuddy/common/controller/BoardController.java
@@ -1,13 +1,11 @@
 package com.github.travelbuddy.common.controller;
 
 import com.github.travelbuddy.board.dto.BoardAllDto;
+import com.github.travelbuddy.board.dto.BoardDetailDto;
 import com.github.travelbuddy.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -26,5 +24,10 @@ public class BoardController {
             @RequestParam(required = false, defaultValue = "createdAt") String sortBy,
             @RequestParam(required = false, defaultValue = "desc") String order) {
         return boardService.getAllBoards(category, startDate, endDate, sortBy, order);
+    }
+
+    @GetMapping("/{postId}")
+    public BoardDetailDto getPostDetails(@PathVariable Integer postId) {
+        return boardService.getPostDetails(postId);
     }
 }
