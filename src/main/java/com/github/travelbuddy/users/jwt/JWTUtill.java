3d5ec8f1b0b847c7497f6bff818409e7655c01d2
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-19 08:56:45 +0900

    Fix: 필요없는 import 삭제

--- src/main/java/com/github/travelbuddy/users/jwt/JWTUtill.java
+++ src/main/java/com/github/travelbuddy/users/jwt/JWTUtill.java
@@ -2,7 +2,6 @@ package com.github.travelbuddy.users.jwt;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.SignatureException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
