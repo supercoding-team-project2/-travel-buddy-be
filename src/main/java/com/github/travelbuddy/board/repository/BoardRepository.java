9c5476f27e1d37c0f73b6bd7728bb13e404cb61e
usnimoj <jominsu0103@naver.com>
2024-07-18 15:17:11 +0900

    Fix : BoardRepository suggestion 삭제 like_count 추가 , likes 정렬 추가

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -11,19 +11,22 @@ import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
-    @Query(value = "SELECT b.id, b.category, b.title, b.summary, b.suggestion, u.name AS author, r.start_at, r.end_at, pi.url AS representative_image " +
+
+    @Query(value = "SELECT b.id, b.category, b.title, b.summary, u.name AS author, r.start_at, r.end_at, pi.url AS representative_image, " +
+                   "COALESCE(l.like_count, 0) AS like_count " +
                    "FROM boards b " +
                    "LEFT JOIN users u ON b.user_id = u.id " +
                    "LEFT JOIN routes r ON b.route_id = r.id " +
                    "LEFT JOIN post_imgs pi ON b.id = pi.post_id " +
+                   "LEFT JOIN (SELECT post_id, COUNT(*) AS like_count FROM likes GROUP BY post_id) l ON b.id = l.post_id " +
                    "WHERE (:category IS NULL OR b.category = :category) " +
-                   "AND (:startDate IS NULL OR :endDate IS NULL OR (DATE(r.start_at) <= DATE(:startDate) AND DATE(r.end_at) >= DATE(:endDate))) " +
+                   "AND (:startDate IS NULL OR :endDate IS NULL OR (DATE(r.start_at) <= DATE(:endDate) AND DATE(r.end_at) >= DATE(:startDate))) " +
                    "AND pi.id = (SELECT pi2.id FROM post_imgs pi2 WHERE pi2.post_id = b.id ORDER BY pi2.id LIMIT 1) " +
                    "ORDER BY " +
                    "CASE WHEN :sortBy = 'createdAt' AND :order = 'desc' THEN b.created_at END DESC, " +
                    "CASE WHEN :sortBy = 'createdAt' AND :order = 'asc' THEN b.created_at END ASC, " +
-                   "CASE WHEN :sortBy = 'suggestion' AND :order = 'desc' THEN b.suggestion END DESC, " +
-                   "CASE WHEN :sortBy = 'suggestion' AND :order = 'asc' THEN b.suggestion END ASC, " +
+                   "CASE WHEN :sortBy = 'likes' AND :order = 'desc' THEN like_count END DESC, " +
+                   "CASE WHEN :sortBy = 'likes' AND :order = 'asc' THEN like_count END ASC, " +
                    "CASE WHEN :sortBy = 'title' AND :order = 'asc' THEN b.title END ASC, " +
                    "CASE WHEN :sortBy = 'title' AND :order = 'desc' THEN b.title END DESC", nativeQuery = true)
     List<Object[]> findAllWithRepresentativeImageAndDateRange(@Param("category") String category, @Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("sortBy") String sortBy, @Param("order") String order);
