26e5d33774271757ff9ac6a3db0a8e25c67da232
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-19 02:33:55 +0900

    Feat: 로그인,jwt 필터 추가

--- src/main/java/com/github/travelbuddy/common/config/SecurityConfig.java
+++ src/main/java/com/github/travelbuddy/common/config/SecurityConfig.java
@@ -1,16 +1,32 @@
 package com.github.travelbuddy.common.config;
 
+import com.github.travelbuddy.users.jwt.JWTFilter;
+import com.github.travelbuddy.users.jwt.JWTUtill;
+import com.github.travelbuddy.users.jwt.LoginFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtill jwtUtill;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     //비밀번호 암호화
     @Bean
@@ -32,10 +48,24 @@ public class SecurityConfig {
                         .requestMatchers("/api/user/signup").permitAll()
                         .anyRequest().authenticated());
 
+//        http
+//                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+
         //jwt는 세션을 stateless로 관리
+
+        //loginFilter전에 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtill), LoginFilter.class);
+
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        LoginFilter loginFilter = new LoginFilter(authenticationManager(authenticationConfiguration),jwtUtill);
+        loginFilter.setFilterProcessesUrl("/api/user/login");
+
+        http
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
