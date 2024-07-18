ffb2795ddc43e74f2160ed72ed11a7472b1a2927
Kim Chanho <beroso20th@gmail.com>
2024-07-18 21:08:39 +0900

    Feat: 메시지전송, 채팅방생성, 메시지불러오기, 메시지/채팅방 DB에 저장

--- src/main/java/com/github/travelbuddy/common/controller/chat/ChatController.java
+++ src/main/java/com/github/travelbuddy/common/controller/chat/ChatController.java
@@ -1,23 +1,51 @@
 package com.github.travelbuddy.common.controller.chat;
 
-import com.github.travelbuddy.chat.dto.ChatMessageRequestDto;
-import com.github.travelbuddy.chat.dto.ChatMessageResponseDto;
+import com.github.travelbuddy.chat.entity.ChatMessage;
+import com.github.travelbuddy.chat.dto.ChatNotification;
+import com.github.travelbuddy.chat.service.ChatMessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@Slf4j
+@RestController
 @RequiredArgsConstructor
 public class ChatController {
     private final SimpMessagingTemplate messagingTemplate;
-//    private final SimpMessageSendingOperations messagingTemplate;
+    private final ChatMessageService chatMessageService;
 
-    @ResponseBody
-    @MessageMapping("/chats")
-    public void message(ChatMessageRequestDto chatMessageRequestDto) {
-        System.out.println("============================= /chat ====================================");
-        ChatMessageResponseDto chatMessageResponseDto = new ChatMessageResponseDto();
+    @MessageMapping("/chat/enter")
+    public void processMessage(@Payload ChatMessage chatMessage) {
+        log.info("=================== /publish/chat ===================");
+
+        ChatMessage savedMessage = chatMessageService.save(chatMessage);
+        log.info("savedMessage = " + savedMessage);
+
+        messagingTemplate.convertAndSendToUser(
+                chatMessage.getRecipientId(), // user
+                "queue/messages",             // destination
+                ChatNotification.builder()    // payload
+                        .id(savedMessage.getId())
+                        .senderId(savedMessage.getSenderId())
+                        .recipientId(savedMessage.getRecipientId())
+                        .content(savedMessage.getContent())
+                        .build()
+        );
+    }
+
+    @GetMapping("/messages/{senderId}/{recipientId}")
+    public ResponseEntity<List<ChatMessage>> findChatMessage(
+            @PathVariable("senderId") String senderId,
+            @PathVariable("recipientId") String recipientId
+    ) {
+        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
     }
 }
