59837eb8b4f3787686fee2beca3b8b8fdf109b9e
usnimoj <jominsu0103@naver.com>
2024-07-18 19:21:19 +0900

    Feat : BoardRepository 게시글 상세정보 native query 작성

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -30,4 +30,25 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
                    "CASE WHEN :sortBy = 'title' AND :order = 'asc' THEN b.title END ASC, " +
                    "CASE WHEN :sortBy = 'title' AND :order = 'desc' THEN b.title END DESC", nativeQuery = true)
     List<Object[]> findAllWithRepresentativeImageAndDateRange(@Param("category") String category, @Param("startDate") Date startDate, @Param("endDate") Date endDate, @Param("sortBy") String sortBy, @Param("order") String order);
+
+
+    @Query(value = "SELECT b.id, b.title, b.summary, b.content, b.category, u.name AS author, " +
+                   "r.start_at, r.end_at, " +
+                   "COALESCE(l.like_count, 0) AS like_count, " +
+                   "pi.url AS image, " +
+                   "rd.day AS route_day, " +
+                   "rdp.place_name AS place_name, " +
+                   "rdp.place_category AS place_category, " +
+                   "t.age_min, t.age_max, t.target_number, t.participant_count, t.gender " +
+                   "FROM boards b " +
+                   "LEFT JOIN users u ON b.user_id = u.id " +
+                   "LEFT JOIN routes r ON b.route_id = r.id " +
+                   "LEFT JOIN post_imgs pi ON b.id = pi.post_id " +
+                   "LEFT JOIN (SELECT post_id, COUNT(*) AS like_count FROM likes GROUP BY post_id) l ON b.id = l.post_id " +
+                   "LEFT JOIN routes_day rd ON r.id = rd.routes_id " +
+                   "LEFT JOIN routes_day_place rdp ON rd.id = rdp.routes_day_id " +
+                   "LEFT JOIN trips t ON b.id = t.post_id " +
+                   "WHERE b.id = :postId " +
+                   "GROUP BY b.id, b.title, b.summary, b.content, b.category, u.name, r.start_at, r.end_at, l.like_count, pi.url, rd.day, rdp.place_name, rdp.place_category, t.age_min, t.age_max, t.target_number, t.participant_count, t.gender", nativeQuery = true)
+    List<Object[]> findPostDetailsById(@Param("postId") Integer postId);
 }
