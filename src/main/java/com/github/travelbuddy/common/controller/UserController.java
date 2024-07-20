14404fc5ed4e666464871dd465d3067a96aaeaa7
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-21 06:59:52 +0900

    Feat: 유저 정보 수정기능 controller 추가

--- src/main/java/com/github/travelbuddy/common/controller/UserController.java
+++ src/main/java/com/github/travelbuddy/common/controller/UserController.java
@@ -1,19 +1,12 @@
 package com.github.travelbuddy.common.controller;
 
-import com.github.travelbuddy.users.dto.CustomUserDetails;
-import com.github.travelbuddy.users.dto.SignupDto;
-import com.github.travelbuddy.users.dto.SignupResponse;
-import com.github.travelbuddy.users.dto.UserResponse;
-import com.github.travelbuddy.users.jwt.JWTFilter;
-import com.github.travelbuddy.users.repository.UserRepository;
+import com.github.travelbuddy.users.dto.*;
 import com.github.travelbuddy.users.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,19 +16,20 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<SignupResponse> signup(SignupDto signupDto){
+    public ResponseEntity<UserResponse> signup(SignupDto signupDto){
         return userService.signup(signupDto);
     }
 
 
     @GetMapping("/")
-    public ResponseEntity<UserResponse> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UserResponse response = userService.getUserInfo(userDetails.getUserId());
+    public ResponseEntity<UserInfoResponse> getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserInfoResponse response = userService.getUserInfo(userDetails.getUserId());
         return ResponseEntity.ok(response);
     }
 
-    @PutMapping("/")
-    public ResponseEntity<SignupResponse> updateUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        return userService.updateUserInfo(userDetails.getUserId());
+    @PutMapping("/profile-picture")
+    public ResponseEntity<UserResponse> updateUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                       @RequestParam("profilePicture") MultipartFile profilePicture) {
+        return userService.updateUserInfo(userDetails.getUserId(),profilePicture);
     }
 }
