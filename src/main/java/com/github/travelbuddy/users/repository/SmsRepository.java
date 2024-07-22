6ca6ef99dbd9b13f1cc4d84842c6f06343fdb410
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-22 23:29:31 +0900

    Fix: String 형태로 받기 위해 쿼리문 추가

--- src/main/java/com/github/travelbuddy/users/repository/SmsRepository.java
+++ src/main/java/com/github/travelbuddy/users/repository/SmsRepository.java
@@ -2,10 +2,12 @@ package com.github.travelbuddy.users.repository;
 
 import com.github.travelbuddy.users.entity.SmsEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SmsRepository extends JpaRepository<SmsEntity, Integer> {
+    @Query("SELECT code FROM SmsEntity WHERE phoneNum= :phoneNum")
     String findByPhoneNum(String phoneNum);
     void deleteByPhoneNum(String phoneNum);
 }
