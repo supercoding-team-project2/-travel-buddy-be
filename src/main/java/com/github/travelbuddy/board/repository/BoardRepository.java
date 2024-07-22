c95c0cfc994112c8d966c737344a437e43184951
usnimoj <jominsu0103@naver.com>
2024-07-23 05:33:26 +0900

    Feat : query routeId & tripId 추가

--- src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
+++ src/main/java/com/github/travelbuddy/board/repository/BoardRepository.java
@@ -33,13 +33,13 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
 
 
     @Query(value = "SELECT b.id, b.title, b.summary, b.content, b.category, u.name AS author, " +
-                   "r.start_at, r.end_at, " +
+                   "r.id, r.start_at, r.end_at, " +
                    "COALESCE(l.like_count, 0) AS like_count, " +
                    "pi.url AS image, " +
                    "rd.day AS route_day, " +
                    "rdp.place_name AS place_name, " +
                    "rdp.place_category AS place_category, " +
-                   "t.age_min, t.age_max, t.target_number, t.participant_count, t.gender " +
+                   "t.id, t.age_min, t.age_max, t.target_number, t.participant_count, t.gender " +
                    "FROM boards b " +
                    "LEFT JOIN users u ON b.user_id = u.id " +
                    "LEFT JOIN routes r ON b.route_id = r.id " +
@@ -49,7 +49,7 @@ public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
                    "LEFT JOIN routes_day_place rdp ON rd.id = rdp.routes_day_id " +
                    "LEFT JOIN trips t ON b.id = t.post_id " +
                    "WHERE b.id = :postId " +
-                   "GROUP BY b.id, b.title, b.summary, b.content, b.category, u.name, r.start_at, r.end_at, l.like_count, pi.url, rd.day, rdp.place_name, rdp.place_category, t.age_min, t.age_max, t.target_number, t.participant_count, t.gender", nativeQuery = true)
+                   "GROUP BY b.id, b.title, b.summary, b.content, b.category, u.name, r.id, r.start_at, r.end_at, l.like_count, pi.url, rd.day, rdp.place_name, rdp.place_category, t.id, t.age_min, t.age_max, t.target_number, t.participant_count, t.gender", nativeQuery = true)
     List<Object[]> findPostDetailsById(@Param("postId") Integer postId);
 
     @Query("SELECT b.id, b.title, b.summary, pi.url, b.category , b.createdAt " +
