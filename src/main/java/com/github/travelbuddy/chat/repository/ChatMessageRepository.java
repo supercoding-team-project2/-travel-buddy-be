186555130d3d5a6d5ce7ecb1a5f020fd6ef5ee6f
Kim Chanho <beroso20th@gmail.com>
2024-07-24 13:26:37 +0900

    Refactor: 채팅관련 entity id 다시 변경

--- src/main/java/com/github/travelbuddy/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/github/travelbuddy/chat/repository/ChatMessageRepository.java
@@ -3,8 +3,9 @@ package com.github.travelbuddy.chat.repository;
 import com.github.travelbuddy.chat.entity.ChatMessage;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.math.BigInteger;
 import java.util.List;
 
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, BigInteger> {
     List<ChatMessage> findByChatId(String s);
 }
--- src/main/java/com/github/travelbuddy/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/github/travelbuddy/chat/repository/ChatRoomRepository.java
@@ -4,8 +4,9 @@ package com.github.travelbuddy.chat.repository;
 import com.github.travelbuddy.chat.entity.ChatRoom;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
-public interface ChatRoomRepository extends MongoRepository<ChatRoom, String> {
+public interface ChatRoomRepository extends MongoRepository<ChatRoom, BigInteger> {
     Optional<ChatRoom> findBySenderIdAndRecipientId(String senderId, String recipientId);
 }
