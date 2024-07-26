ba2bceb503e8b4e0de57513f438d0ce3a0db71ea
choihohyun <choihyeon@gmali.com>
2024-07-26 14:00:55 +0900

    Docs     : log 작성

--- src/main/java/com/github/travelbuddy/common/controller/RouteController.java
+++ src/main/java/com/github/travelbuddy/common/controller/RouteController.java
@@ -7,6 +7,8 @@ import com.github.travelbuddy.routes.service.RouteDeleteService;
 import com.github.travelbuddy.routes.service.RouteGetService;
 import com.github.travelbuddy.routes.service.RoutePutService;
 import com.github.travelbuddy.users.dto.CustomUserDetails;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -17,6 +19,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/routes")
 public class RouteController {
+
+    private static final Logger log = LoggerFactory.getLogger(RouteController.class);
+
 
     private final RoutePostService routePostService;
     private final RouteDeleteService routeDeleteService;
@@ -38,15 +43,21 @@ public class RouteController {
             Integer userId = userDetails.getUserId();
 
             if (routeDto != null && routeId == null) {
+                log.info("새로운 경로 생성");
                 RouteDto createdRoute = routePostService.createRouteWithDaysAndPlaces(routeDto, userId);
+                log.info("생성된 경로 DTO: {}", createdRoute);
                 return ResponseEntity.ok(createdRoute);
             } else if (routeDto != null && routeId != null && !routeDto.getDays().isEmpty()) {
+                log.info("기존 경로에 새로운 날과 장소 추가");
                 RouteDto updatedRoute = routePostService.addRouteDayAndPlaces(routeId, routeDto);
+                log.info("업데이트된 경로 DTO: {}", updatedRoute);
                 return ResponseEntity.ok(updatedRoute);
             } else {
+                log.warn("잘못된 요청입니다. routeDto: {}, routeId: {}", routeDto, routeId);
                 throw new IllegalArgumentException("잘못된 요청입니다.");
             }
         } catch (Exception e) {
+            log.error("오류 발생: ", e);
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
