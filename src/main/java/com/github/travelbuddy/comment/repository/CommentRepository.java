12130f35104edb6e06a307bd5ba0b8411b2cf512
Kim Chanho <beroso20th@gmail.com>
2024-07-28 00:44:55 +0900

    Refactor: CommentEntity 필드 수정 post->board

--- src/main/java/com/github/travelbuddy/comment/repository/CommentRepository.java
+++ src/main/java/com/github/travelbuddy/comment/repository/CommentRepository.java
@@ -1,10 +1,11 @@
 package com.github.travelbuddy.comment.repository;
 
+import com.github.travelbuddy.board.entity.BoardEntity;
 import com.github.travelbuddy.comment.entity.CommentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
-    List<CommentEntity> findAllByPostId(Integer post_id);
+    List<CommentEntity> findAllByBoard(BoardEntity boardEntity);
 }
