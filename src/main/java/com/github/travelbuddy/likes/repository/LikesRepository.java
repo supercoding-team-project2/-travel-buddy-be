cf9c7cf3ba954ebc08686e6f8ccc03ea2b984d28
Kim Chanho <beroso20th@gmail.com>
2024-07-28 21:53:09 +0900

    Refactor: 좋아요 등록 로직 수정

--- src/main/java/com/github/travelbuddy/likes/repository/LikesRepository.java
+++ src/main/java/com/github/travelbuddy/likes/repository/LikesRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikesRepository extends JpaRepository<LikesEntity, Integer> {
     LikesEntity findByUserIdAndBoardId(Integer userId, Integer boardId);
-    boolean existsByUserId(Integer userId);
+    boolean existsByUserIdAndBoardId(Integer userId, Integer boardId);
 
     void deleteAllByBoard(BoardEntity board);
 }
