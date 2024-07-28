443ec9bdab785ad29c80470106063aa72734d958
HyeYoung-Hwang <114129207+HyeYoung-Hwang@users.noreply.github.com>
2024-07-28 10:05:15 +0900

    Feat: 소셜로그인(카카오) 기능 구현

---
+++ src/main/java/com/github/travelbuddy/users/dto/oauth2/OAuth2UserDto.java
@@ -0,0 +1,22 @@
+package com.github.travelbuddy.users.dto.oauth2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OAuth2UserDto {
+//    private String role;
+//    private String name;
+//    private String username;
+    private Integer userId;
+    private String name;
+    private String email;
+//    private String residentNum;
+//    private String phoneNum;
+//    //todo: Gender type으로 할건지
+//    private String gender;
+//    //todo: password, profile, created_at
+
+
+}
