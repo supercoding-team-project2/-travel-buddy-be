aba6cb38bc44eea37558d65d40de7f78cf6a71aa
Kim Chanho <beroso20th@gmail.com>
2024-07-29 21:16:09 +0900

    Feat: 채팅방입장 response

---
+++ src/main/java/com/github/travelbuddy/chat/response/ChatRoomOpenResponse.java
@@ -0,0 +1,14 @@
+package com.github.travelbuddy.chat.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatRoomOpenResponse {
+    private String chatRoomId;
+}
