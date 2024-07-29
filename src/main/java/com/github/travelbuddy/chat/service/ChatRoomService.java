b01dabbdbf52906bfb46b12f859da2df3f4c7073
Kim Chanho <beroso20th@gmail.com>
2024-07-29 21:15:47 +0900

    Refactor: 채팅방생성로직 수정

--- src/main/java/com/github/travelbuddy/chat/service/ChatRoomService.java
+++ src/main/java/com/github/travelbuddy/chat/service/ChatRoomService.java
@@ -2,6 +2,7 @@ package com.github.travelbuddy.chat.service;
 
 import com.github.travelbuddy.chat.entity.ChatRoom;
 import com.github.travelbuddy.chat.repository.ChatRoomRepository;
+import com.github.travelbuddy.common.util.UUIDUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -24,24 +25,30 @@ public class ChatRoomService {
                 });
     }
 
-    private String createChatId(String senderId, String recipientId) {
-        String chatId = String.format("%s_%s", senderId, recipientId);
+    private String createChatId(String myId, String opponentId) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(myId);
+        sb.append("_");
+        sb.append(opponentId);
+        sb.append("_");
+        sb.append(UUIDUtil.generateUUIDFromUserId(Integer.valueOf(myId)));
+        String chatRoomId = sb.toString();
 
         ChatRoom senderRecipient = ChatRoom.builder()
-                .chatId(chatId)
-                .senderId(senderId)
-                .recipientId(recipientId)
+                .chatId(chatRoomId)
+                .senderId(myId)
+                .recipientId(opponentId)
                 .build();
 
         ChatRoom recipientSender = ChatRoom.builder()
-                .chatId(chatId)
-                .senderId(recipientId)
-                .recipientId(senderId)
+                .chatId(chatRoomId)
+                .senderId(opponentId)
+                .recipientId(myId)
                 .build();
 
         chatRoomRepository.save(senderRecipient);
         chatRoomRepository.save(recipientSender);
 
-        return chatId;
+        return chatRoomId;
     }
 }
