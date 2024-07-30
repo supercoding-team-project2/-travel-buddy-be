067ff75900c129ce85a298e5bf4ffeb713d2e9e0
Kim Chanho <beroso20th@gmail.com>
2024-07-30 21:58:08 +0900

    Refactor: 필드변경

--- src/main/java/com/github/travelbuddy/chat/entity/ChatMessage.java
+++ src/main/java/com/github/travelbuddy/chat/entity/ChatMessage.java
@@ -1,11 +1,12 @@
 package com.github.travelbuddy.chat.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
 import java.util.Date;
+
 
 @Getter
 @Setter
@@ -16,10 +17,13 @@ import java.util.Date;
 @Document
 public class ChatMessage {
     @Id // id를 String, ObjectId, BigInteger 중 하나로 사용해야 함
-    private BigInteger id;
+    private String id;
     private String chatId;
-    private String senderName;
-    private String recipientName;
+    private String senderId;
+    private String opponentId;
     private String content;
-    private Date timestamp;
+
+    // TODO : 날짜 값 / 타입 변경......
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+    private Date timeStamp;
 }
